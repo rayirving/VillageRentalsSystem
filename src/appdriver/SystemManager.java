0d0e5c6890ff8d2e5c15311ebccf753101f59fc3
Ray Irving <rayirving.dev@gmail.com>
2026-04-17 23:55:51 -0600

    Added classes file structure and implemented Customer Class

--- src/appdriver/SystemManager.java
+++
@@ -1,5 +0,0 @@
-package appdriver;
-
-public class SystemManager {
-
-}
---
+++ src/domain/Customer.java
@@ -0,0 +1,44 @@
+package domain;
+
+public class Customer {
+	String id;
+	String firstName;
+	String surname;
+	
+	String phoneNumber;
+	String email; 
+	
+	String notes;
+	boolean isBanned = false;
+	
+	
+	// --- Constructor ---
+	
+	public Customer(String id) {
+		this.id = id;
+	}
+	
+	
+	// --- Getters & Setters ---
+
+	public String getId() {return id;}
+
+	public String getFirstName() {return firstName;}
+	public Customer setFirstName(String firstName) {this.firstName = firstName; return this;}
+
+	public String getSurname() {return surname;}
+	public Customer setSurname(String surname) {this.surname = surname; return this;}
+
+	public String getPhoneNumber() {return phoneNumber;}
+	public Customer setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber; return this;}
+
+	public String getEmail() {return email;}
+	public Customer setEmail(String email) {this.email = email; return this;}
+
+	public String getNotes() {return notes;}
+	public void setNotes(String notes) {this.notes = notes;}
+
+	public boolean isBanned() {return isBanned;}
+	public void setBanned(boolean isBanned) {this.isBanned = isBanned;}
+	
+}
