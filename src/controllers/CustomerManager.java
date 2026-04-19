781630b9186749fa5062d63acf4f3ba46b74264d
Ray Irving <rayirving.dev@gmail.com>
2026-04-19 15:50:06 -0600

    Implemented CustomerManager and CustomerDAO

--- src/controllers/CustomerManager.java
+++ src/controllers/CustomerManager.java
@@ -1,99 +1,51 @@
 package controllers;
 
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.sql.SQLException;
+import java.util.List;
 import domain.Customer;
+import persistence.CustomerDAO;
 
 public class CustomerManager {
 	
-	private ArrayList<Customer> customers = new ArrayList<>();
+	private List<Customer> customers;
+	private final CustomerDAO customerDAO = new CustomerDAO();
 	
-	/*
-	 * get customer by id
-	 * returns null if no customer is found
-	 */
-	public Customer getCustomer(String id) {
+	public CustomerManager() throws SQLException {
+		load();
+	}
+	
+	private void load() throws SQLException {
+		customers = customerDAO.loadAll();
+	}
+	
+	public List<Customer> getAll() {
+		return customers;
+	}
+	
+	public Customer getById(String id) {
 		for (Customer customer : customers) {
 			if (customer.getId().equals(id)) {
 				return customer;
 			}
 		}
-		System.out.println("Customer not found!");
-		return null; 
+		return null;
 	}
 	
-	public void addCustomer(Customer customer) {
-		customers.add(customer);
+	public void add(Customer customer) throws SQLException {
+		customerDAO.add(customer);
+		load();
 	}
 	
-	/*
-	 * remove customer by id
-	 */
-	public void removeCustomer(String id) {
-		for (int i = 0; i < customers.size();i ++) {
-			if (customers.get(i).getId().equals(id)) {
-				customers.remove(i);
-				System.out.println("Customer removed.");
-				return;
-			}
-		}
-		System.out.println("Customer not found!");
+	public void update(Customer customer) throws SQLException {
+		customerDAO.update(customer);
+		load();
 	}
 	
-	/*
-	 * modify customer by id
-	 */
-	public void modifyCustomer(String id) { // might need to add some error handling
-		Customer customer = getCustomer(id);
-		
-		//print menu
-		System.out.println("What would you like to modify?");
-		System.out.println("1. First name");
-		System.out.println("2. Last name");
-		System.out.println("3. Phone number");
-		System.out.println("4. Email");
-		System.out.println("5. Notes");
-		System.out.println("6. Banned Status");
-		
-		//getting input
-		Scanner keyboard;
-		keyboard = new Scanner(System.in);
-		int choice = Integer.parseInt(keyboard.nextLine());
-		
-		switch (choice) {
-		case 1: 
-			System.out.println("Enter new first name: ");
-			customer.setFirstName(keyboard.nextLine());
-			break;
-		case 2:
-			System.out.println("Enter new last name: ");
-			customer.setSurname(keyboard.nextLine());
-			break;
-		case 3:
-			System.out.println("Enter new phone number: ");
-			customer.setPhoneNumber(keyboard.nextLine());
-			break;
-		case 4:
-			System.out.println("Enter new email: ");
-			customer.setEmail(keyboard.nextLine());
-			break;
-		case 5:
-			System.out.println("Enter new notes: ");
-			customer.setNotes(keyboard.nextLine());
-			break;
-		case 6:
-			System.out.println("1. Banned");
-			System.out.println("2. Unbanned");
-			int ban_choice = Integer.parseInt(keyboard.nextLine());
-			if (ban_choice == 1) {
-				customer.setBanned(true);
-			}
-			else if (ban_choice == 2) {
-				customer.setBanned(false);
-			}
-			break;
-		}
+	public void remove(String id) throws SQLException {
+		customerDAO.remove(id);
+		load();
 	}
+	
+
 }
