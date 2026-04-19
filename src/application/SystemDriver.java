80ad79789cfff27d47995e05924c3d5bce9ba814
Ray Irving <rayirving.dev@gmail.com>
2026-04-19 15:54:33 -0600

    added Managers to SystemDriver

--- src/application/SystemDriver.java
+++ src/application/SystemDriver.java
@@ -2,6 +2,9 @@ package application;
 
 import java.sql.SQLException;
 
+import controllers.CustomerManager;
+import controllers.InventoryManager;
+import controllers.RentalManager;
 import presentation.UIManager;
 
 import persistence.DatabaseManager;
@@ -12,8 +15,16 @@ public class SystemDriver {
 	public SystemDriver() {
 		try {
 			DatabaseManager.initDatabase();
+			
+			CustomerManager customerManager = new CustomerManager();
+			InventoryManager inventoryManager = new InventoryManager();
+			RentalManager rentalManager = new RentalManager();
+			
+			
 			UIManager userInterface = new UIManager();
 			userInterface.start();
+			
+			
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
